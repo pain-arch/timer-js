20bf222879e376d153af9fd7d13846983de455de
pain-arch <himulol123@gmail.com>
2021-09-17 14:07:29 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const newYear = '1 jan 2022';
+
+const getDays = document.getElementById('days');
+const getHours = document.getElementById('hours');
+const getMins = document.getElementById('mins');
+const getSeconds = document.getElementById('seconds'); 
+
+function timer(){
+
+    const newYearDate = new Date(newYear);
+    const currentDate = new Date();
+
+    const totalSeconds = (newYearDate - currentDate) / 1000;
+
+    const days = Math.floor(totalSeconds / 3600 /24);
+    const hours = Math.floor((totalSeconds / 3600) % 24);
+    const min = Math.floor((totalSeconds / 60)%60);
+    const sec = Math.floor((totalSeconds)%60);
+
+    getDays.innerText = days;
+    getHours.innerText = hours;
+    getMins.innerText = min;
+    getSeconds.innerText = sec;
+
+    console.log(days, hours, min, sec);
+};
+
+timer();
+
+setInterval(timer, 1000);
